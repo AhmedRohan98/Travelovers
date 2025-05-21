c03c8080a79908597b99f20e24f3d63d50bc559d
salman-alvi2k <m.salmanalvi2k@gmail.com>
2025-05-21 11:30:17 +0500

    refactor: simplify tourism page component to return null

--- src/app/tourism/page.tsx
+++ src/app/tourism/page.tsx
@@ -1,12 +1,4 @@
-// "use client";
-
-// import Countries from "@/components/Countries";
-// import { tourismCountries } from "@/lib/data/countries";
-
-// export default function StudyPage() {
-//   return (
-//     <div className="container mx-auto py-8">
-//       <Countries countries={tourismCountries} category="tourism" />
-//     </div>
-//   );
-// }
+export default function TourismPage() {
+    return null;
+  }
+  
